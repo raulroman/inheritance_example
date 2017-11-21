f85281bcdc636e39788a63a258f53aee3f21f230
RaulRoman <raulromanjr@gmail.com>
2017-11-20 22:15:24 -0500

    add cell phones to store

---
+++ store_item.rb
@@ -0,0 +1,29 @@
+# Store selling cell phones
+
+iphone = {
+    model: "X", 
+    color: "Space Grey",
+    capacity: 256,
+    price: 1150
+}
+
+samsung = {
+    model: "Galaxy",
+    color: "Champagne Gold",
+    capacity: 128,
+    price: 900
+
+}
+
+google = {
+    model: "Pixel",
+    color: "White",
+    capacity: 64,
+    price: 1000
+}
+
+p "#{iphone[:model]}"
+
+p "#{samsung[:model]}"
+
+p "#{google[:model]}"
